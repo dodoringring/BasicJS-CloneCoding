11edf6e5d333a29595c6647530be8ebe0bdc7951
dodoringring <dodohee1504@gmail.com>
2023-10-06 09:47:58 +0900

    first commit

---
+++ app/src/main/cloneCoding/todo.js
@@ -0,0 +1,33 @@
+const toDoForm=document.querySelector(".js-toDoForm"),
+  toDoInput=toDoForm.querySelector("input");
+  toDoList=document.querySelector(".js-toDoList");
+
+const TODOS_LS='toDos';
+
+function paintToDo(text) {
+  console.log(text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue=toDoInput.value;
+  paintToDo(currentValue);
+}
+function loadToDos() {
+  const toDos=localStorage.getItem(TODOS_LS);
+  if(toDos!==null){
+
+  }else{
+
+  }
+
+}
+
+
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit)
+}
+
+init();
